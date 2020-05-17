897b75f12cdcca570e20c7d5e16fb7277e29835c
Eran Cohen <eran@stratoscale.com>
2020-05-17 11:13:15 +0300

    Fixed install command

--- internal/host/installcmd.go
+++ internal/host/installcmd.go
@@ -48,7 +48,7 @@ func (i *installCmd) GetStep(ctx context.Context, host *models.Host) (*models.St
 		role = RoleBootstrap
 	}
 
-	const cmdArgsTmpl = `-c "sudo podman run -v /dev:/dev:rw -v /opt:/opt:rw --privileged --pid=host  {{.INSTALLER}} --role {{.ROLE}}  --cluster-id {{.CLUSTER_ID}}  --host {{.HOST}} --port {{.PORT}} --boot-device {{.BOOT_DEVICE}} --host-id {{.HOST_ID}} --openshift-version {{.OPENSHIFT_VERSION}}""`
+	const cmdArgsTmpl = `-c "sudo podman run -v /dev:/dev:rw -v /opt:/opt:rw --privileged --pid=host  {{.INSTALLER}} --role {{.ROLE}}  --cluster-id {{.CLUSTER_ID}}  --host {{.HOST}} --port {{.PORT}} --boot-device {{.BOOT_DEVICE}} --host-id {{.HOST_ID}} --openshift-version {{.OPENSHIFT_VERSION}}"`
 	t, err := template.New("cmd").Parse(cmdArgsTmpl)
 	if err != nil {
 		return nil, err
