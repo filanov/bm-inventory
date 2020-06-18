0286f36038c6c6758f63db8b086581fa52f8f886
Ori Amizur <oamizur@redhat.com>
2020-06-18 21:56:28 +0300

    Swagger generate

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -1732,6 +1732,35 @@ func init() {
         "$ref": "#/definitions/event"
       }
     },
+    "free_addresses_request": {
+      "type": "array",
+      "items": {
+        "type": "string",
+        "pattern": "^([0-9]{1,3}\\.){3}[0-9]{1,3}\\/[0-9]|[1-2][0-9]|3[0-2]?$"
+      }
+    },
+    "free_network_addresses": {
+      "type": "object",
+      "properties": {
+        "free_addresses": {
+          "type": "array",
+          "items": {
+            "type": "string",
+            "format": "ipv4"
+          }
+        },
+        "network": {
+          "type": "string",
+          "pattern": "^([0-9]{1,3}\\.){3}[0-9]{1,3}\\/[0-9]|[1-2][0-9]|3[0-2]?$"
+        }
+      }
+    },
+    "free_networks_addresses": {
+      "type": "array",
+      "items": {
+        "$ref": "#/definitions/free_network_addresses"
+      }
+    },
     "host": {
       "type": "object",
       "required": [
@@ -2176,7 +2205,8 @@ func init() {
         "connectivity-check",
         "execute",
         "inventory",
-        "install"
+        "install",
+        "free-network-addresses"
       ]
     },
     "steps": {
@@ -3931,6 +3961,35 @@ func init() {
         "$ref": "#/definitions/event"
       }
     },
+    "free_addresses_request": {
+      "type": "array",
+      "items": {
+        "type": "string",
+        "pattern": "^([0-9]{1,3}\\.){3}[0-9]{1,3}\\/[0-9]|[1-2][0-9]|3[0-2]?$"
+      }
+    },
+    "free_network_addresses": {
+      "type": "object",
+      "properties": {
+        "free_addresses": {
+          "type": "array",
+          "items": {
+            "type": "string",
+            "format": "ipv4"
+          }
+        },
+        "network": {
+          "type": "string",
+          "pattern": "^([0-9]{1,3}\\.){3}[0-9]{1,3}\\/[0-9]|[1-2][0-9]|3[0-2]?$"
+        }
+      }
+    },
+    "free_networks_addresses": {
+      "type": "array",
+      "items": {
+        "$ref": "#/definitions/free_network_addresses"
+      }
+    },
     "host": {
       "type": "object",
       "required": [
@@ -4375,7 +4434,8 @@ func init() {
         "connectivity-check",
         "execute",
         "inventory",
-        "install"
+        "install",
+        "free-network-addresses"
       ]
     },
     "steps": {
