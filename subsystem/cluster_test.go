b783c5dd8e53a05f90560ff81bcf8647b91f00b4
Michael Filanov <mfilanov@redhat.com>
2020-06-29 10:18:01 +0300

    Fix subsystem after removing install_started_at and install_completed_at

--- subsystem/cluster_test.go
+++ subsystem/cluster_test.go
@@ -865,22 +865,16 @@ var _ = Describe("cluster install, with default network params", func() {
 	It("[only_k8s]install cluster", func() {
 		clusterID := *cluster.ID
 		registerHostsAndSetRoles(clusterID, 3)
+
+		_, err := bmclient.Installer.InstallCluster(ctx, &installer.InstallClusterParams{ClusterID: clusterID})
+		Expect(err).NotTo(HaveOccurred())
+
 		rep, err := bmclient.Installer.GetCluster(ctx, &installer.GetClusterParams{ClusterID: clusterID})
 		Expect(err).NotTo(HaveOccurred())
 		c := rep.GetPayload()
-		startTimeInstalling := c.InstallStartedAt
-		startTimeInstalled := c.InstallCompletedAt
-
-		_, err = bmclient.Installer.InstallCluster(ctx, &installer.InstallClusterParams{ClusterID: clusterID})
-		Expect(err).NotTo(HaveOccurred())
-
-		rep, err = bmclient.Installer.GetCluster(ctx, &installer.GetClusterParams{ClusterID: clusterID})
-		Expect(err).NotTo(HaveOccurred())
-		c = rep.GetPayload()
 		Expect(swag.StringValue(c.Status)).Should(Equal("installing"))
 		Expect(swag.StringValue(c.StatusInfo)).Should(Equal("Installation in progress"))
 		Expect(len(c.Hosts)).Should(Equal(3))
-		Expect(c.InstallStartedAt).ShouldNot(Equal(startTimeInstalling))
 		for _, host := range c.Hosts {
 			Expect(swag.StringValue(host.Status)).Should(Equal("installing"))
 		}
@@ -890,10 +884,6 @@ var _ = Describe("cluster install, with default network params", func() {
 		}
 
 		waitForClusterState(ctx, clusterID, "installed", 10*time.Second, "installed")
-		rep, err = bmclient.Installer.GetCluster(ctx, &installer.GetClusterParams{ClusterID: clusterID})
-		Expect(err).NotTo(HaveOccurred())
-		c = rep.GetPayload()
-		Expect(c.InstallCompletedAt).ShouldNot(Equal(startTimeInstalled))
 	})
 })
 
